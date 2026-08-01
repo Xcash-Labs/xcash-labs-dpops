3e28d1143a088d7776e25c34496926f626e299df
John D <az0006t@protonmail.com>
2026-08-01 18:30:40 -0400

    Add temporary checks until delegate limit is reached

--- src/xcash_delegates/xcash_delegates.c
+++ src/xcash_delegates/xcash_delegates.c
@@ -135,9 +135,36 @@ int read_organize_delegates(delegates_t* delegates, size_t* delegates_count_resu
         }
       }
 
+      // The following checks are temporary until we have enough delegates.
+
       if (!skip_delegate &&
-          strlen(delegates[delegate_index].public_address) > 0 &&
-          strlen(delegates[delegate_index].IP_address) > 0) {
+          strcmp(delegates[delegate_index].delegate_type, "solo") == 0 &&
+          delegates[delegate_index].total_vote_count < 500000)
+      {
+          WARNING_PRINT(
+              "Skipping solo delegate %s: requires at least 500000 votes (current: %" PRIu64 ")",
+              delegates[delegate_index].public_address,
+              delegates[delegate_index].total_vote_count);
+
+          // skip_delegate = true;
+      }
+
+      if (!skip_delegate &&
+          strcmp(delegates[delegate_index].delegate_type, "shared") == 0 &&
+          delegates[delegate_index].delegate_fee > 25 &&
+          delegates[delegate_index].total_vote_count < 500000)
+      {
+          WARNING_PRINT(
+              "Skipping shared delegate %s: charging more than 25%% requires at least 500000 votes (current: %" PRIu64 ")",
+              delegates[delegate_index].public_address,
+              delegates[delegate_index].total_vote_count);
+
+          // skip_delegate = true;
+      }
+
+      if (!skip_delegate &&
+        strlen(delegates[delegate_index].public_address) > 0 &&
+        strlen(delegates[delegate_index].IP_address) > 0) {
         delegate_index++;
       }
       
